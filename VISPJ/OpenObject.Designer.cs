17f00a1fbfd7d24d933c25f171e7642c0cd7a8ba
hanjae <lovelyhyun@gmail.com>
2014-05-19 11:50:24 +0900

    Add object background selection UI
    Add object background selection UI

--- VISPJ/OpenObject.Designer.cs
+++ VISPJ/OpenObject.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -38,11 +39,14 @@
             this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Location = new System.Drawing.Point(13, 29);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(259, 207);
+            this.pictureBox1.Size = new System.Drawing.Size(259, 191);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
             // 
             // button1
             // 
@@ -55,17 +59,28 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "배경을 선택해 주세요.";
+            // 
             // OpenObject
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "OpenObject";
             this.Text = "OpenObject";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -73,5 +88,6 @@
 
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
--- VISPJ/OpenObject.cs
+++ VISPJ/OpenObject.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,6 +17,8 @@ namespace VISPJ
         public OpenObject(Form1 theForm)
         {
             callerForm = theForm;
+            alphaPen = new Pen(Color.Purple, 10);
+            alphaPen.SetLineCap(LineCap.Round, LineCap.Round, DashCap.Flat);
             InitializeComponent();
         }
         public void setImage(Image theImage)
@@ -36,6 +39,41 @@ namespace VISPJ
                 callerForm.setObjectPicture(this.pictureBox1.Image);
             }
             this.Close();
+        }
+
+        private Point? _Previous = null;
+        private Pen alphaPen;
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            _Previous = e.Location;
+            pictureBox1_MouseMove(sender, e);
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_Previous != null)
+            {
+                if (pictureBox1.Image == null)
+                {
+                    Bitmap bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                    using (Graphics g = Graphics.FromImage(bmp))
+                    {
+                        g.Clear(Color.White);
+                    }
+                    pictureBox1.Image = bmp;
+                }
+                using (Graphics g = Graphics.FromImage(pictureBox1.Image))
+                {
+                    g.DrawLine(alphaPen, _Previous.Value, e.Location);
+                }
+                pictureBox1.Invalidate();
+                _Previous = e.Location;
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            _Previous = null;
         }
     }
 }
